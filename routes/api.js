152c6d639452ddcba82c75b21f57dd94e2830d67
Alex Quintero <robquint@gmail.com>
2016-12-01 20:11:32 -0700

    fake form posts

--- routes/api.js
+++ routes/api.js
@@ -2,28 +2,36 @@ var express = require('express');
 var router = express.Router();
 var request = require('request');
 
-router.post('/api/:zone/:volume', function(req, res, next){
-
-    var headers = {
-        'User-Agent': 'WHA/0.0.1', 
-        'Content-Type': 'application/x-www-form-urlencoded'
-    };
+router.post('/:zone/:volume', function(req, res, next){
 
     var options = {
         url: 'http://192.168.1.249/keypad.html',
         method: 'POST',
-        headers: headers,
-        data: 'SrcDropDown='+ zone + '&VolDropDown=' + volume + '&_isgrp=0&_grpmsk=0&_grpsrc=0&_zsrc=12&_cznum=0'
+        headers: {
+            'Upgrade-Insecure-Requests': '1',
+            'Origin': 'http://192.168.1.249'
+        },
+        form: {
+            SrcDropDown: req.params.zone,
+            VolDropDown: req.params.volume,
+            _isgrp: 0,
+            _grpmsk: 0,
+            _grpsrc: 0,
+            _zsrc: 12,
+            _cznum: 0
+            }
     };
 
-    request(options, function(error, response, body){
-        if (!error && response.statusCode == 200){
-            console.log('yipee');
-        }
-    });
+    request(options, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+        // Print out the response body
+        console.log(body)
+    }
 })
+        
+});
 
-router.get('/api', function(req, res, next){
+router.get('/', function(req, res, next){
     res.send('yo');
 })
 
